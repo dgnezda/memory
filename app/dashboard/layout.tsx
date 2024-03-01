4a458cb083b760e526e60439d79516fc1f155072
Domen <domen.gnezda@pm.me>
2024-03-02 00:28:38 +0100

    Smaller sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from "../ui/sidenav";
 export default function Layout({ children }: { children: React.ReactNode}) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
+            <div className="w-full flex-none md:w-[166px]">
                 <SideNav />
             </div>
             <div className="flex h-screen w-screen items-center justify-center">{children}</div>
