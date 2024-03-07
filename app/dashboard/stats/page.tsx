7b1218c1c86fb40a21755affc5079741b670af9c
Domen <domen.gnezda@pm.me>
2024-03-07 22:07:00 +0100

    Remove quotes

--- app/dashboard/stats/page.tsx
+++ app/dashboard/stats/page.tsx
@@ -10,19 +10,19 @@ export default function Page() {
             <WhiteDiv value="Best Scores">
                 <div className="text-xs mt-2">
                     <div>Overall: 10</div>
-                    <div>Game mode "numbers": 10</div>
-                    <div>Game mode "letters": 11</div>
-                    <div>Game mode "symbols": 12</div>
-                    <div>Game mode "arrows": 15</div>
+                    <div>Game mode numbers: 10</div>
+                    <div>Game mode letters: 11</div>
+                    <div>Game mode symbols: 12</div>
+                    <div>Game mode arrows: 15</div>
                 </div>
             </WhiteDiv>
             <WhiteDiv value="Average Scores">
                 <div className="text-xs mt-2">
                     <div>Overall: 15</div>
-                    <div>Game mode "numbers": 13</div>
-                    <div>Game mode "letters": 14</div>
-                    <div>Game mode "symbols": 15</div>
-                    <div>Game mode "arrows": 17</div>
+                    <div>Game mode numbers: 13</div>
+                    <div>Game mode letters: 14</div>
+                    <div>Game mode symbols: 15</div>
+                    <div>Game mode arrows: 17</div>
                 </div>
             </WhiteDiv>
             <WhiteDiv value="Best Time">
